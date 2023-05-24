de6764e92b19e1aaa760a229e79fa45f87573ee8
Andrzej <andrzuk@gmail.com>
2023-05-24 10:11:52 +0200

    Init web page template.

--- MySeries/src/app/app.module.ts
+++ MySeries/src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgChartsModule } from 'ng2-charts';
+import { NgxBootstrapIconsModule, allIcons } from 'ngx-bootstrap-icons';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgChartsModule,
+    NgxBootstrapIconsModule.pick(allIcons)
   ],
   providers: [],
   bootstrap: [AppComponent]
