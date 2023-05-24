9b6e465218ebe9a7cdcdc2fda033c156466f33cc
Andrzej <andrzuk@gmail.com>
2023-05-24 11:51:43 +0200

    Implement harmonic frequencies.

--- MySeries/src/app/panel/panel.component.ts
+++ MySeries/src/app/panel/panel.component.ts
@@ -31,8 +31,11 @@ export class PanelComponent {
     const distance = parseInt(this.inputForm.controls.distance.value || '0'); 
     this.appComponent.seriesData = [];
     for (var i = 0; i <= points; i++) {
-      const x = 2 * i * this.PI / points;
-      const u = Math.sin(x);
+      const x = distance * i * this.PI / points;
+      var u = 0;
+      for (var j = 0; j < harmonic; j++) {
+        u += 1 / (2 * j + 1) * Math.sin((2 * j + 1) * x);
+      }
       this.appComponent.seriesData.push({ id: i, value: u });
     }
     this.actionService.updateActionMessage('generate');
