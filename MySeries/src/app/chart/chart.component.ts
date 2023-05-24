4f62e476455b4f7547e0c8f307c5d93c9ef8089d
Andrzej <andrzuk@gmail.com>
2023-05-24 11:16:29 +0200

    Prepare panel and chart components.

--- MySeries/src/app/chart/chart.component.ts
+++ MySeries/src/app/chart/chart.component.ts
@@ -1,10 +1,100 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { ChartConfiguration, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+
+import { AppComponent } from '../app.component'; 
+
+import { ActionsService } from '../actions.service';
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.scss']
 })
-export class ChartComponent {
+export class ChartComponent implements OnInit {
 
+  @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
+
+  sampleData: any = [
+    { id: 0, value: 0 }, { id: 1, value: 0 }, { id: 2, value: 0 }, { id: 3, value: 0 }, { id: 4, value: 0 }, { id: 5, value: 0 }, { id: 6, value: 0 }, 
+  ];
+
+  constructor(public appComponent: AppComponent, private actionService: ActionsService) { }
+
+  ngOnInit(): void {
+    this.actionService.currentActionStageMessage.subscribe(msg => { 
+      switch (msg) {
+        case 'init':
+          this.initData();
+          break;
+        case 'generate':
+          this.updateData();
+          break;
+        default:
+          break;
+      }
+    });
+  }
+
+  initData() {
+    this.seriesChartData.labels = [];
+    this.seriesChartData.datasets[0].data = [];
+    this.sampleData.forEach((item: any) => {
+      this.seriesChartData.labels?.push(item.id);
+      this.seriesChartData.datasets[0].data.push(item.value);
+    });
+    this.chart?.update();
+  }
+
+  updateData() {
+    this.seriesChartData.labels = [];
+    this.seriesChartData.datasets[0].data = [];
+    this.appComponent.seriesData.forEach((item: any) => {
+      this.seriesChartData.labels?.push(item.id);
+      this.seriesChartData.datasets[0].data.push(item.value);
+    });
+    this.chart?.update();
+  }
+
+  public seriesChartData: ChartConfiguration['data'] = {
+    datasets: [
+      {
+        data: [],
+        backgroundColor: 'rgba(250, 25, 0, 0.3)',
+        borderColor: 'rgba(250, 25, 0, 1)',
+        pointBackgroundColor: 'rgba(250, 25, 0, 1)',
+        pointBorderColor: 'rgba(255, 255, 255, 1)',
+        pointHoverBackgroundColor: 'rgba(255, 255, 255, 1)',
+        pointHoverBorderColor: 'rgba(250, 25, 0, 1)',
+        fill: 'origin',
+      }
+    ],
+    labels: []
+  };
+
+  public seriesChartOptions: ChartConfiguration['options'] = {
+    elements: {
+      line: {
+        tension: 0.5
+      }
+    },
+    scales: {
+      x: {},
+      y: {
+        position: 'left',
+        grid: {
+          color: 'rgba(100, 100, 100, 0.3)',
+        },
+        ticks: {
+          color: 'rgba(100, 100, 100, 1)'
+        }
+      },
+    },
+    maintainAspectRatio: false,
+    plugins: {
+      legend: { display: false },
+    } 
+  };
+
+  public seriesChartType: ChartType = 'line';
 }
--- MySeries/src/app/panel/panel.component.ts
+++ MySeries/src/app/panel/panel.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms'; 
+
+import { AppComponent } from '../app.component'; 
+
+import { ActionsService } from '../actions.service';
 
 @Component({
   selector: 'app-panel',
@@ -7,4 +12,18 @@ import { Component } from '@angular/core';
 })
 export class PanelComponent {
 
+  inputForm = new FormGroup({
+    points: new FormControl('200'),
+    harmonic: new FormControl('10'),
+  }); 
+
+  constructor(public appComponent: AppComponent, private actionService: ActionsService) { }
+
+  ngOnInit(): void {
+    this.actionService.updateActionMessage('init');
+  }
+ 
+  generateClick() {
+    
+  }
 }
